308356c5f39d0ebc71e1c8ea09d17354ca8ec520
Shafin Ahmed <79255498+ahshafin2315@users.noreply.github.com>
2025-05-10 22:43:23 +0600

    fix: Missing socket-io import and event listeners
    - event listener for verification approvals for contribution points update
    - socket-io initialization for run.py

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
-from app import app, db
+from app import app, db, socketio
 from app.models.user import User
 from werkzeug.security import generate_password_hash
 
 def create_default_users():
+    if not User.query.filter_by(email="admin@test.com").first():
+        admin = User(
+            name="Admin User",
+            email="admin@test.com",
+            password=generate_password_hash("admin123"),
+            is_admin=True,
+            contact_info="Admin Contact"
+        )
+        db.session.add(admin)
+
     if not User.query.filter_by(email="user@test.com").first():
         user = User(
             name="Test User",
@@ -12,11 +22,11 @@ def create_default_users():
             contact_info="Test User Contact"
         )
         db.session.add(user)
-    
+
     db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         create_default_users()
-    app.run(host='0.0.0.0', debug=True)	
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
